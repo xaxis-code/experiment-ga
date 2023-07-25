65f80182706d96f4337ec3ff358cde7f7139f5f3
Yaakov Yekezkel <Yaakov.Yekezkel@xaxis.com>
2023-07-25 19:30:43 -0400

    liniting

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-# -*- coding: iso-8859-1 -*-
-
-import os
 import random
 from flask import Flask, request, jsonify
 
@@ -9,13 +6,13 @@ app = Flask(__name__)
 # Define a route for the hello endpoint
 @app.route('/hello', methods=['GET'])
 def hello():
-    my_input = request.args.get('name')
-    lucky_numbers = sorted(random.sample(range(1, 71), 7))
-    lucky_numbers_str = ", ".join(str(n) for n in lucky_numbers)
-    my_output = f"Hello {my_input}, your lucky numbers are: {lucky_numbers_str}"
+  my_input = request.args.get('name')
+  lucky_numbers = sorted(random.sample(range(1, 71), 7))
+  lucky_numbers_str = ", ".join(str(n) for n in lucky_numbers)
+  my_output = f"Hello {my_input}, your lucky numbers are: {lucky_numbers_str}"
 
-    return jsonify({'output': my_output})
+  return jsonify({'output': my_output})
 
 # Run the app in debug mode
 if __name__ == '__main__':
-    app.run(port=12345)
+  app.run(port=12345)
