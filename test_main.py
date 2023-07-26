669a4995bcf585f7f5488cb048d0ed40bf5816ee
Yaakov Yekezkel <Yaakov.Yekezkel@xaxis.com>
2023-07-26 09:12:30 -0400

    linting correction

--- test_main.py
+++ test_main.py
@@ -2,11 +2,13 @@ import pytest
 from flask import json
 from main import app
 
+
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
+
 
 def test_hello(client):
     sets = []
